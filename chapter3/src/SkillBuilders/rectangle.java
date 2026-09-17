fc47af724fe95b657caa381094596fcbe539e411
510132008 <510132008@L815WD220033.edu.cbe.ab.ca>
2026-09-17 10:01:46 -0600

    10:01am

--- chapter3/src/SkillBuilders/rectangle.java
+++ chapter3/src/SkillBuilders/rectangle.java
@@ -2,7 +2,7 @@ package SkillBuilders;
 
 import java.util.Scanner;
 
-public class rectangle 
+public class rectangle
 {
 
 	public static void main(String[] args)
@@ -13,6 +13,7 @@ public class rectangle
 		
 		//Create a Scanner
 		Scanner userinput= new Scanner(System.in);
+	
 		
 		//Get the width value from the user
 		width = userinput.nextInt();
@@ -27,9 +28,20 @@ public class rectangle
 		//Display the Width and Length on the console
 		System.out.print("The width is: "+ width);
 		System.out.print("The length is: "+ length);
-
 		//Abhay
+		
+		/**
+		 * Calculates and displays the area of a rectangle
+		 */
+		 int area; //calculated area of rectangle
+		 area = length * width;
+		 System.out.println("Area: " + area);
+		 }
+
+        
+
+		
 		
 	}
 
-}
+
